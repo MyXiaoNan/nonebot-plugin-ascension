6540eeaa0a3e3292fd0ee286cc50bc9dc1becfd6
KomoriDev <mute231010@gmail.com>
2024-07-19 17:27:25 +0800

    :sparkles: 添加 extension

---
+++ nonebot_plugin_ascension/extension.py
@@ -0,0 +1,27 @@
+from nonebot_plugin_htmlrender import text_to_pic
+from nonebot_plugin_alconna.uniseg import UniMessage
+from nonebot.internal.adapter import Bot, Event, Message
+from nonebot_plugin_alconna.extension import Extension, add_global_extension
+
+from .config import TEMPLATE_DIR
+
+
+class TextToImageExtension(Extension):
+
+    @property
+    def priority(self) -> int:
+        return 10
+
+    @property
+    def id(self) -> str:
+        return "text_to_image"
+
+    async def send_wrapper(
+        self, bot: Bot, event: Event, send: str | Message | UniMessage
+    ):
+        return UniMessage.image(
+            raw=await text_to_pic(str(send), css_path=str(TEMPLATE_DIR / "message.css"))
+        )
+
+
+add_global_extension(TextToImageExtension())
