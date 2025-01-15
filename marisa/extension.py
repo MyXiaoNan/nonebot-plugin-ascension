396c9f5955f9e4c5911fc83523beb356e2939109
KomoriDev <mute231010@gmail.com>
2025-01-15 22:03:22 +0800

    :bug: 修复多个消息段转图片后显示异常的问题

--- marisa/extension.py
+++ marisa/extension.py
@@ -1,6 +1,6 @@
 from nonebot_plugin_htmlrender import text_to_pic
-from nonebot_plugin_alconna.uniseg import UniMessage
 from nonebot.internal.adapter import Bot, Event, Message
+from nonebot_plugin_alconna.uniseg import Text, UniMessage
 from nonebot_plugin_alconna.extension import Extension, add_global_extension
 
 from .configs import TEMPLATE_DIR, config
@@ -18,11 +18,20 @@ class TextToImageExtension(Extension):
     async def send_wrapper(
         self, bot: Bot, event: Event, send: str | Message | UniMessage
     ):
-        if config.basic.send_with_image:
-            return UniMessage.image(
-                raw=await text_to_pic(
-                    str(send), css_path=str(TEMPLATE_DIR / "message.css")
+        plain_text = (
+            send.extract_plain_text()
+            if isinstance(send, Message | UniMessage)
+            else send
+        )
+        extra_segment = send.exclude(Text) if isinstance(send, UniMessage) else send
+        if config.basic.send_with_image and plain_text != "":
+            return (
+                UniMessage.image(
+                    raw=await text_to_pic(
+                        plain_text, css_path=str(TEMPLATE_DIR / "message.css")
+                    )
                 )
+                + extra_segment
             )
         return send
 
